3824ef18726a836fdaac94539890f87c6482df3c
jacky <s9751842@gmail.com>
2023-12-29 00:56:10 +0800

    first commit

---
+++ APP/commit_test/231006_2.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+	int num1=123;
+	double num2=4.567;
+	printf("%d is an integer\n",num1);
+	printf("%f is a double\n",num2);
+	system("pause");
+	return 0;
+}

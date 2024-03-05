5d777701c03e07cbc6f8464dea09e512097eb132
JackChen5566 <147010437+JackChen5566@users.noreply.github.com>
2024-03-05 11:20:12 +0800

    update readme and remove unnessary import

--- APP/Django/myapp/views.py
+++ APP/Django/myapp/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from datetime import datetime
-from bottle import route, run
 from gpiozero import LED
 from gpiozero import AngularServo
 from time import sleep

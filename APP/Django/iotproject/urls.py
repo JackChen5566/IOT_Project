759bf9012094174e04efef3d8647091df892b4aa
Ching Xiong <139099490+Ching-Xong@users.noreply.github.com>
2024-03-19 13:38:52 +0800

    temperature

--- APP/Django/iotproject/urls.py
+++ APP/Django/iotproject/urls.py
@@ -1,19 +1,12 @@
 from django.contrib import admin
 from django.urls import path
-<<<<<<< HEAD
 from myapp.views import index, toggle, control_servo,pump_run
-
-=======
 from myapp.views import index, toggle, control_servo, temperature_data
->>>>>>> tem
 
 urlpatterns = [
     path('', index, name='index'),
     path('<int:led>/', toggle, name='toggle'),
     path('control_servo/', control_servo, name='control_servo'),
-<<<<<<< HEAD
-    path('WaterPump/',pump_run,name='WaterPump')
-=======
-    path('temperature/', temperature_data)
->>>>>>> tem
-]
+    path('WaterPump/',pump_run,name='WaterPump'),
+    path('temperature/', temperature_data),
+    ]
